042a4547416b3d18cc04663075cb4e1ece8c3748
suraj.kumar1 <suraj.kumar1@juspay.in>
2023-05-15 02:42:19 +0530

    DB config added

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,6 @@
 // config for db
+const mongoose = require('mongoose');
+// mongoose.connect("mongodb://localhost:27017/studentNukkadDB", {useNewUrlParser : true, useUnifiedTechnology : true});
+mongoose.connect("mongodb://localhost:27017/studentNukkadDB", {useNewUrlParser : true})
+.then( () => console. log( "connection successfull"))
+.catch((err) => console. log (err));
